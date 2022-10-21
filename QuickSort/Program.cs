90b474ef1fd6ba993f19b813a4b8d77088c7b3f1
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-10-21 16:54:06 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //Array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison
+        private int mov_count = 0; //number of data movements
+
+        //Number of elements in array
+        private int n;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
